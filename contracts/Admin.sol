6eb67b61791656fd265a8da25cc15e1cea626b90
Riki <rikkki@tenping.kr>
2023-09-11 00:47:01 +0900

    feat: add function to initialize lending (#3)
    Co-authored-by: Nick <nick@Nicks-MacBook-Air.local>

--- contracts/Admin.sol
+++ contracts/Admin.sol
@@ -1,9 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.18;
 
+import { ILocker } from "./interfaces/ILocker.sol";
+import { ILockerFactory } from "./interfaces/ILockerFactory.sol";
+import { ILoanFactory, LoanInfo } from "./interfaces/ILoanFactory.sol";
+
 contract Admin {
     address public owner;
+    ILockerFactory public lockerFactory;
+    ILoanFactory public loanFactory;
     mapping(address borrower => bool isWhitelisted) public borrowers;
+
 
     modifier onlyOwner() {
         require(msg.sender == owner, "unauthorized");
@@ -12,6 +19,14 @@ contract Admin {
 
     constructor() {
         owner = msg.sender;
+    }
+
+    function setFactories(
+        address _lockerFactory,
+        address _loanFactory
+    ) external onlyOwner {
+        lockerFactory = ILockerFactory(_lockerFactory);
+        loanFactory = ILoanFactory(_loanFactory);
     }
 
     function addBorrower(address _address) external onlyOwner {
@@ -25,5 +40,22 @@ contract Admin {
     function setOwner(address _address) external onlyOwner {
         require(_address != address(0), "invalid address");
         owner = _address;
+    }
+
+    function createProposal(uint256[7] memory conditions, address _asset) external {
+        address lockerAddress = lockerFactory.createLocker(_asset);
+        LoanInfo memory loanInfo = LoanInfo(
+            address(this),
+            msg.sender,
+            ILocker(lockerAddress),
+            conditions[0],
+            conditions[1],
+            conditions[2],
+            conditions[3],
+            conditions[4],
+            conditions[5],
+            conditions[6]
+        );
+        address loanAddress = loanFactory.createLoan(loanInfo);
     }
 }
--- contracts/LoanFactory.sol
+++ contracts/LoanFactory.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.18;
 
-import { Loan } from "./Loan.sol";
+import { Loan, LoanInfo } from "./Loan.sol";
 
 contract LoanFactory {
     mapping(uint256 => address) public loans;
@@ -12,7 +12,7 @@ contract LoanFactory {
         admin = _admin;
     }
 
-    function createLoan(Loan.LoanInfo memory _conditions) external returns (address) {
+    function createLoan(LoanInfo memory _conditions) external returns (address) {
         Loan loan = new Loan(_conditions);
         loans[loanSize++] = address(loan);
         return address(loan);
